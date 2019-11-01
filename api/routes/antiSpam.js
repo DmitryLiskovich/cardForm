47d2b4708278936cede4544059d4283e6cd14177
DmitryLiskovich <dliskovich@VRP.local>
2019-11-01 16:59:58 +0300

    'Server'

--- api/routes/antiSpam.js
+++ api/routes/antiSpam.js
@@ -5,8 +5,10 @@ function antiSpam(req, res, next){
 	connection.query(
 		`SLECT * FROM antiSpam WHERE ip='${req.connection.adress}'`,
 		(err, response) => {
+		console.log(response);
 		  if(response.length){
 			  if(Date() - response[0].request_date < 1200000){
+				console.log(Date() - response[0].request_date);
 				  return res.status(400).json({message: 'Try in 20 min'});
 			  }else{
 				  connection.query(`UPDATE antiSpam SET request_date=${Date()} WHERE ip='${req.connection.adress}'`);
