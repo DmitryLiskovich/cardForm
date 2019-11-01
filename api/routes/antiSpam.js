5881d3c2fbc061e220a39e7dfb8f22a32431b46a
DmitryLiskovich <dliskovich@VRP.local>
2019-11-01 17:04:29 +0300

    'Server'

--- api/routes/antiSpam.js
+++ api/routes/antiSpam.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const connection = require("./dbconfig");
 
 function antiSpam(req, res, next){
+	console.log(req.connection.remoteAddress);
 	connection.query(
 		`SLECT * FROM antiSpam WHERE ip='${req.connection.remoteAddress}'`,
 		(err, response) => {
-		console.log(response);
 		  if(response.length){
 			  if(Date() - response[0].request_date < 1200000){
 				console.log(Date() - response[0].request_date);
