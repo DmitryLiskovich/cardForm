a01aac28d63d13a6d0eb714c6a8d97c7dc9857a9
DmitryLiskovich <dliskovich@VRP.local>
2019-11-01 12:46:51 +0300

    'Server'

--- api/routes/sendEmail.js
+++ api/routes/sendEmail.js
@@ -4,37 +4,19 @@ const mailer = require('sendmail')({silent: true});
 
 router.post('/', (req, res)=>{
 	console.log(req.body);
-	// sendEmail(req.body.text).catch(err=> console.log(err))
 	mailer({
-		from: 'test@yourdomain.com',
-		to: 'dimlis199586@gmail.com',
-		subject: 'MailComposer sendmail',
-		html: 'Mail of test sendmail '
+		from: '"dmitryliskovich.github.io" <dmitry.liskovich@github.com>',
+		to: 'dimalis199586@gmail.com',
+		subject: 'Message from github page',
+		html: `<p style='font-style: italic, font-size: 18px'>${req.body.text}</p>`
 	}, (err, repl)=>{
-		console.log(err.message)
 		if(err){
+			console.log(err);
 			return res.status(400).json({message: err});
+		}else{
+			return res.status(200).json({message: 'Ok'});
 		}
-  		// console.dir(reply)
 	})
-	
-
-	// async function sendEmail(body){
-	// 	let transporter = mailer.createTransport('SMTP',{
-	// 		host: 'smtp.mail.ru',
-	// 		port: 465,
-	// 		auth: {
-	// 			user: "dima_liskovich@mail.ru",
-	// 			pass: '40752'
-	// 		}
-			
-
-	// 	});
-	// 	let info = await transporter.sendMail({
-			
-	// 	})
-	// 	return res.status(200).json({message: 'OK'});
-	// }
 });
 
 module.exports = router;
