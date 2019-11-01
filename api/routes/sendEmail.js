4c52f2d1cf4e5941a82506c307028635e20f9858
DmitryLiskovich <dliskovich@VRP.local>
2019-11-01 15:45:11 +0300

    'Server'

--- api/routes/sendEmail.js
+++ api/routes/sendEmail.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const mailer = require('sendmail')({silent: true});
 
 router.post('/', (req, res)=>{
-	console.log(req.body);
+	console.log(req.url);
 	mailer({
 		from: '"dmitryliskovich" <dmitry.liskovich@github.com>',
 		to: 'dimalis199586@gmail.com',
 		subject: `${req.body.them}`,
-		html: `<h2>${req.body.them}</h2><p style='font-style: italic; font-size: 18px'>${req.body.text}</p>`
+		html: `<h1>${req.body.them}</h1><p style='font-style: italic; font-size: 18px'>${req.body.text}</p>`
 	}, (err, repl)=>{
 		if(err){
 			console.log(err);
