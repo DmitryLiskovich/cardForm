5f7a4cf0fd516a34278e0af9baf8b28f23a50a92
DmitryLiskovich <dliskovich@VRP.local>
2019-12-27 11:47:06 +0300

    'Server'

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ server.listen(port, ()=>{
 	const port = server.address().port;
 	console.log("Express is open on port " + port);
 });
+
+const io = require('socket.io').listen(3001);
+
+io.on('connection', (socket)=>{
+	socket.on('message', (message)=>{
+		socket.broadcast.send(message)
+	})
+	socket.on('disconnect', ()=>{
+		socket.send({message:`User has been disconnected`, type: 'connect'});
+	})
+})
