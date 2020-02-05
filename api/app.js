86288a889e057592689e048fb13cfc31d139739a
Dmitry Liskovich <Dima_liskovich@mail.ru>
2020-02-05 19:29:03 +0300

    'Server'

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ app.disable("x-powered-by");
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
