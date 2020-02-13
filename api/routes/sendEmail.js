7fb6835a29bab4d0282c557bf43255dab3aef08d
DmitryLiskovich <dliskovich@VRP.local>
2020-02-13 16:17:42 +0300

    Revert "'Server'"
    This reverts commit 641170ce680b59ad2aba2dfc307845737264606c.

--- api/routes/sendEmail.js
+++ api/routes/sendEmail.js
@@ -15,11 +15,7 @@ router.get('/', antiSpam, (req, res)=>{
 	return res.status(200).json({message: 'ok'});
 })
 
-<<<<<<< HEAD
-router.post('/', (req, res)=>{
-=======
 router.post('/',  (req, res)=>{
->>>>>>> parent of 641170c... 'Server'
 	transporter.sendMail({
 		from: '"dmitryliskovich" <dmitry.liskovich@github.com>',
 		to: 'dimalis199586@gmail.com',
